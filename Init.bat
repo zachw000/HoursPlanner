168b9000046c933d80cfd095752ce0783e909710
Zachary Waldron <waldrzac000@gmail.com>
2018-02-06 16:33:45 -0700

    Updated resources

--- Init.bat
+++ Init.bat
@@ -1,2 +1,5 @@
 @echo off
 npm install
+npm install -g electron-prebuilt
+npm install -g bower
+bower install bootstrap
--- Init.sh
+++ Init.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 echo Starting NPM Initialization
 npm install
+npm install -g electron-prebuilt
+npm install -g bower
+bower install bootstrap
