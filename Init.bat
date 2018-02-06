0bb8723a05bd69fd32363d873c2fe4bb0dc08a61
Zachary Waldron <waldrzac000@gmail.com>
2018-02-06 16:53:43 -0700

    Adding more web dependencies
    Setting up a project takes E F F O R T.

--- Init.bat
+++ Init.bat
@@ -2,4 +2,6 @@
 npm install
 npm install -g electron-prebuilt
 npm install -g bower
+npm install -g @angular/cli
+npm install --save jquery
 bower install bootstrap
--- Init.sh
+++ Init.sh
@@ -3,4 +3,6 @@ echo Starting NPM Initialization
 npm install
 npm install -g electron-prebuilt
 npm install -g bower
+npm install -g @angular/cli
+npm install --save jquery
 bower install bootstrap
