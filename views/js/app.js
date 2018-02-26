d2db19ea37f6442364f6b17d71e6c6f270fe97c7
Zachary Waldron <waldrzac000@gmail.com>
2018-02-25 22:38:18 -0700

    Added Login Check
    Removed IE detection.

--- views/js/app.js
+++ views/js/app.js
@@ -34,50 +34,6 @@ var stringMonth = function(monthInteger) {
         }
 };
 
-/**
- * detect IE
- * returns version of IE or false, if browser is not Internet Explorer
- */
-function detectIE() {
-  var ua = window.navigator.userAgent;
-
-  // Test values; Uncomment to check result …
-
-  // IE 10
-  // ua = 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)';
-
-  // IE 11
-  // ua = 'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko';
-
-  // Edge 12 (Spartan)
-  // ua = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36 Edge/12.0';
-
-  // Edge 13
-  // ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586';
-
-  var msie = ua.indexOf('MSIE ');
-  if (msie > 0) {
-    // IE 10 or older => return version number
-    return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
-  }
-
-  var trident = ua.indexOf('Trident/');
-  if (trident > 0) {
-    // IE 11 => return version number
-    var rv = ua.indexOf('rv:');
-    return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
-  }
-
-  var edge = ua.indexOf('Edge/');
-  if (edge > 0) {
-    // Edge (IE 12+) => return version number
-    return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
-  }
-
-  // other browser
-  return false;
-}
-
 var getCookie = (cname, callback) => {
   var value = {name: cname};
   ses.cookies.get(value, (error, cookies) => {
@@ -117,11 +73,21 @@ var setLoginCookie = (cookie_inf, callback) => {
 
 var loggedIn = (callback) => {
   getCookie("name", (error, cookies) => {
-    if (error || cookies[0] === undefined)
-      return callback("Not logged in", false);
+    if (error !== null || cookies[0] === undefined) {
+      if (error !== null)
+        return callback(null, false, null);
+      else
+        return callback(error, false, null);
+    }
 
+    if (cookies[0] !== undefined && cookies[0].hasOwnProperty('value')) {
+      // Return true with the name
+      return callback(null, true, cookies[0].value);
+    }
   });
 };
+
+var AnsiDate = (mmddyyy) => {};
 
 
 var d = new Date();
@@ -145,11 +111,15 @@ jQuery.fn.rotate = function(degrees) {
     return $(this);
 };
 
-getCookie("name", (error, cookies) => {
-  alert(cookies[0].value);
-});
-
 $(document).ready(function () {
+  loggedIn((err, li, name) => {
+    if (err !== null) {
+      console.error(err);
+    } else if (li) {
+      document.title = "Hours Planner - Welcome " + name;
+    }
+  });
+
   $('.nav-rotate').on('click', function() {
           degreesRotated += 180;
           degreesRotated %= 360;
@@ -157,12 +127,6 @@ $(document).ready(function () {
   });
 
   $('.date-anc').text(dateObj.month + " " + dateObj.day + ", " + dateObj.year);
-
-  var ie = detectIE();
-  /*ONLY USED IN WEBSITE MODE, WITHOUT NODE INTEGRATIONS*/
-  if (ie <= 10 && ie != false) {
-    $("body").html("<h1 class='text-danger'>Sorry, this browser is not supported; please use a newer browser such as Chrome.</h1>");
-  }
 
   if (currentPage() === "index.ejs") {
     // This code will only execute on index.ejs
