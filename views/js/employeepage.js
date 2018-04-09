9e4da332a8a9292c034af9e15aca6d53ee517cde
Zachary Waldron <waldrzac000@gmail.com>
2018-04-09 16:33:51 -0700

    Employees Filtered properly now
    Not only was the code updated, but so many bugs were fixed that it isn't even recognizable as the same file anymore.

--- views/js/employeepage.js
+++ views/js/employeepage.js
@@ -3,93 +3,20 @@
 var dphx = true;
 var dlax = true;
 var projmanager = false;
-
-var filterEmployeeData = (key, dval) => {
-  // This function will rely on data already being loaded into RAM
-  var empSet = record.employees.Employees;
-  var newEmpSet = [];
-  for (var i = 0; i < empSet.length; i++) {
-    if (empSet[i][key] !== dval) newEmpSet.push(empSet[i]);
-  }
-
-  return newEmpSet;
-};
-
-var matchEmployeeData = (key, dval) => {
-  // This function will rely on data already being loaded into RAM
-  var empSet = record.employees.Employees;
-  var newEmpSet = [];
-  for (var i = 0; i < empSet.length; i++) {
-    if (empSet[i][key] === dval) newEmpSet.push(empSet[i]);
-  }
-
-  return newEmpSet;
-};
-
 $(document).ready(function() {
-  // Run when page is loaded.
-  $('#division-phx').change(function() {
-    dphx = this.checked;
-    var empRows = $("#empTable").html().split('\n');
-    if (!dphx) {
-      var empSet = filterEmployeeData("division", "PHX");
-      var tableRow = [];
-      for (var i = 0; i < empSet.length; i++) {
-        tableRow.push("<td>" + empSet[i].name + "</td><td>" +
-        empSet[i].division + "</td><td>" + empSet[i].role + "</td><td>" + totalHours(empSet[i].hours) +
-        "</td>");
-        tableRow[tableRow.length - 1] = "<tr>" + tableRow[tableRow.length - 1] + "</tr>\n";
-      }
-      $("#empTable").html(tableRow.join(''));
-    } else {
-      var empSet = matchEmployeeData("division", "PHX");
-      var tableRow = [];
-      for (var i = 0; i < empSet.length; i++) {
-        tableRow.push("<td>" + empSet[i].name + "</td><td>" +
-        empSet[i].division + "</td><td>" + empSet[i].role + "</td><td>" + totalHours(empSet[i].hours) +
-        "</td>");
-        tableRow[tableRow.length - 1] = "<tr>" + tableRow[tableRow.length - 1] + "</tr>\n";
-        for (var j = 0; j < empRows.length; j++) {
-          if (tableRow[tableRow.length - 1] == empRows[j])
-            tableRow.pop();
-        }
-      }
-      $("#empTable").html(empRows.join('\n') + tableRow.join(''));
-    }
-    //alert("Filter PHX changed");
+  $("input[type='checkbox']").on('change', function () {
+    dlax = document.getElementById("division-lax").checked;
+    dphx = document.getElementById("division-phx").checked;
+    let nset = record.employees.Employees.filter((element) => {
+      let re = false;
+      if (dlax) re = element.division.toLowerCase() == "lax";
+      if (dphx && !re)re = element.division.toLowerCase() == "phx";
+      return re;
+    }).map((data) => {
+      return `<tr><td>${data.name}</td><td>${data.division}</td><td>${data.role}</td><td>${totalHours(data.hours)}</td></tr>`;
+    }).join("\n");
+    $("#empTable").html(nset);
   });
-
-  $('#division-lax').change(function() {
-    dlax = this.checked;
-    var empRows = $("#empTable").html().split('\n');
-    if (!dlax) {
-      var empSet = filterEmployeeData("division", "LAX");
-      var tableRow = [];
-      for (var i = 0; i < empSet.length; i++) {
-        tableRow.push("<td>" + empSet[i].name + "</td><td>" +
-        empSet[i].division + "</td><td>" + empSet[i].role + "</td><td>" + totalHours(empSet[i].hours) +
-        "</td>");
-        tableRow[tableRow.length - 1] = "<tr>" + tableRow[tableRow.length - 1] + "</tr>\n";
-      }
-      $("#empTable").html(tableRow.join(''));
-    } else {
-      var empSet = matchEmployeeData("division", "LAX");
-      var tableRow = [];
-      for (var i = 0; i < empSet.length; i++) {
-        tableRow.push("<td>" + empSet[i].name + "</td><td>" +
-        empSet[i].division + "</td><td>" + empSet[i].role + "</td><td>" + totalHours(empSet[i].hours) +
-        "</td>");
-        tableRow[tableRow.length - 1] = "<tr>" + tableRow[tableRow.length - 1] + "</tr>\n";
-        for (var j = 0; j < empRows.length; j++) {
-          if (tableRow[tableRow.length - 1] == empRows[j])
-            tableRow.pop();
-        }
-      }
-      $("#empTable").html(tableRow.join('') + empRows.join('\n'));
-    }
-    //alert("Filter LAX changed");
-  });
-
   eventEmitter.on('empRead', () => {
     var empSet = record.employees.Employees;
     var tableRow = [];
@@ -100,11 +27,9 @@ $(document).ready(function() {
       "</td>");
       tableRow[tableRow.length - 1] = "<tr>" + tableRow[tableRow.length - 1] + "</tr>\n";
     }
-
     $("#empTable").html(tableRow.join(''));
   });
 });
-
 $(document).ready(() => {
   // Check the login status
   eventEmitter.on('loggedIn', () => {
@@ -127,7 +52,6 @@ $(document).ready(() => {
       //if (projmanager) alert(lname + " is a project manager. RECORDSET");
     }
   });
-
   if (record.employees === null || typeof record.employees !== 'object') {
     readEmployees((err, ret) => {
       eventEmitter.emit('empRead');
