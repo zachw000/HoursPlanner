fc97036afbcb4f9f5421d90be2bedaa2ce88ceb2
Zachary Waldron <waldrzac000@gmail.com>
2018-04-13 17:00:55 -0700

    Working on project adding/editing

--- views/js/projects.js
+++ views/js/projects.js
@@ -1,14 +1,22 @@
 Array.prototype.dataMap = function() {
     return this.map((data) => {
-        return `<tr><td>${data.projnum}</td><td>${data.name}</td><td>${data.projname}</td><td>${data.active ? "<i class='fa fa-check'></i>" : "<i class='fa fa-times'></i>"}</td></tr>`
+        return `<tr><th scope="row">${data.projnum}</th><td>${data.name}</td><td>${data.projname}</td><td>${data.active ? "<i class='fa fa-check-circle'></i>" : "<i class='fa fa-times'></i>"}</td></tr>`
     }).join('\n')
 }
 $(document).ready(() => {
+    selector = $();
     eventEmitter.on('loggedIn', () => {/*LOG IN SPECIFIC ACTIONS*/})
     eventEmitter.on('projRead', () => {
         let tableRows = record.projects.projects.dataMap()
         $("#p_list").html(tableRows)
+        selector = $("tr th, td");
     })
+    /*$("table").on("click", function () {
+        alert($(this).text());
+    });*/
+    $("td").on('click', function() {
+        alert($(this).text())
+      })
     if ((record.projects === null)) {
         readProjects((err, ret) => {
             if (err !== null) return;
