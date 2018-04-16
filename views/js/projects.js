09b4259580419373c493118fdfeb2a753812e2bd
Zachary Waldron <waldrzac000@gmail.com>
2018-04-16 15:58:23 -0700

    Improve Project Page
    Including new click to inteact with the application.

--- views/js/projects.js
+++ views/js/projects.js
@@ -1,22 +1,29 @@
-Array.prototype.dataMap = function() {
+Array.prototype.dataMap = function () {
     return this.map((data) => {
-        return `<tr><th scope="row">${data.projnum}</th><td>${data.name}</td><td>${data.projname}</td><td>${data.active ? "<i class='fa fa-check-circle'></i>" : "<i class='fa fa-times'></i>"}</td></tr>`
+        return `<tr><th scope="row">${data.projnum}</th><td>${data.name}</td><td>${data.projname}</td><td>${data.active ? 
+            "<i class='fa fa-check-circle'></i> Active" : "<i class='fa fa-times'></i> Not Active"}</td></tr>`
     }).join('\n')
 }
 $(document).ready(() => {
-    selector = $();
-    eventEmitter.on('loggedIn', () => {/*LOG IN SPECIFIC ACTIONS*/})
-    eventEmitter.on('projRead', () => {
-        let tableRows = record.projects.projects.dataMap()
-        $("#p_list").html(tableRows)
-        selector = $("tr th, td");
+    eventEmitter.on('notLoggedIn', () => {
+        $("#nlo-modal").modal();
     })
-    /*$("table").on("click", function () {
-        alert($(this).text());
-    });*/
-    $("td").on('click', function() {
-        alert($(this).text())
-      })
+    $("#nlo-modal").on("hidden.bs.modal", () => {
+        window.location.href = "login.ejs";
+    })
+    eventEmitter.on('projRead', () => {
+        let tableRows = record.projects.projects.sort((a, b) => {
+            return a.projnum - b.projnum;
+        }).dataMap()
+        $("#p_list").html(tableRows)
+    })
+    $("#wrapper").on('click', 'tbody tr', function () {
+        alert($(this).html().split("</"))
+        // get all data from the current table row
+    })
+    $("#newProject").on("click", function () {
+        $("#addnew-modal").modal();
+    })
     if ((record.projects === null)) {
         readProjects((err, ret) => {
             if (err !== null) return;
