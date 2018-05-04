89d30fb1ab98b851b95e026417dc2e6ee2882c98
Zachary Waldron <waldrzac000@gmail.com>
2018-05-04 15:00:30 -0700

    Projects work

--- views/js/projects.js
+++ views/js/projects.js
@@ -8,7 +8,59 @@ let c_proj = null,
     c_index = -1,
     getCurrentProject = (element) => {
         return equalObj(c_proj, element)
+    },
+    writeLiveRecord = () => {
+        if (!(record.projects.projects == null)) {
+            jsonfile.writeFile(prepend + filenames[3], record.projects, (err) => {
+                if (err) console.error(err)
+            })
+        }
     }
+// Spawn process as asynchronous function to avoid application locking
+/**
+ * @desc Using the old project number, it moves through the milestones and employee data updating the project number.
+ * @param {string} projn 
+ * @param {number} p_index 
+ */
+async function updateExtern(projn, p_index) {
+    readMilestones((err, ret) => {
+        if (err) {
+            alert("External Data could not be updated! Contact software maintainer.")
+            console.error(err)
+        } else {
+            // No error has occurred.
+            record.milestones.milestones.forEach((element, m_index) => {
+                if (element.projnum == projn)
+                    record.milestones.milestones[m_index].projnum = record.projects.projects[c_index].projnum
+                if (!(record.milestones.milestones == null)) {
+                    // console.log("WRITING")
+                    jsonfile.writeFile(prepend + filenames[1], record.milestones, (err) => {
+                        if (err !== null) console.error(err);
+                    });
+                }
+            })
+        }
+    })
+
+    readEmployees((err, ret) => {
+        if (err) {
+            alert("External Data could not be updated! Contact software maintainer.")
+            console.error(err)
+        } else {
+            record.employees.Employees.forEach((element, e_index) => {
+                element.hours.forEach((h_obj, h_index) => {
+                    if (h_obj.projnum == projn)
+                        record.employees.Employees[e_index].hours[h_index].projnum = record.projects.projects[c_index].projnum
+                    if (!(record.employees.Employees == null)) {                
+                        jsonfile.writeFile(prepend + filenames[0], record.employees, (err) => {
+                            if (err !== null) console.error()
+                        })
+                    }
+                })
+            })
+        }
+    })
+}
 $(document).ready(() => {
     eventEmitter.on('notLoggedIn', () => {
         $("#nlo-modal").modal();
@@ -30,6 +82,12 @@ $(document).ready(() => {
             $("#newSelect").html(htm)
         })
     })
+    eventEmitter.on('updateTable', () => {
+        let tableRows = record.projects.projects.sort((a, b) => {
+            return a.projnum - b.projnum;
+        }).dataMap()
+        $("#p_list").html(tableRows)
+    })
     $("#p_list").on('click', 'tr', function () {
         let num = $(this).find("th").first().text()
         c_proj = getProjectByNum(num)
@@ -46,11 +104,18 @@ $(document).ready(() => {
         } else {
             // continue save operation
             p_set = record.projects.projects
+            let prev_num = p_set[c_index].projnum
             record.projects.projects[c_index].projnum = $("#editProjNum").val()
             record.projects.projects[c_index].projname = $("#editDesc").val()
             record.projects.projects[c_index].name = $("#editSelect").val()
             record.projects.projects[c_index].active = document.getElementById("editActive").checked
             
+            // TODO: if projnum is different, update milestones AND hours
+            if (record.projects.projects[c_index].projnum !== prev_num) {
+                updateExtern(prev_num, c_index)
+            }
+            writeLiveRecord()
+            eventEmitter.emit("updateTable")
         }
     })
     $("#newProject").on("click", function () {
